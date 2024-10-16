ee02e796727a5abc5003d7b37ec2a173c8a624ca
Delafu7 <128633495+Delafu7@users.noreply.github.com>
2024-10-16 09:39:58 +0200

    Update VersionBuena.py

--- VersionBuena.py
+++ VersionBuena.py
@@ -13,7 +13,7 @@
 #Precondicion: El texto introducido debe de estar en castellano, sin embargo si se deseara introducir otro idioma habria que cambiar los porcentajes de aparicion
 #
 
-def convertidorAnalisisFrecuencia(texto.upper()):
+def convertidorAnalisisFrecuencia(texto):
     contador = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0, "G": 0, "H": 0, "I": 0, "J": 0, "K": 0, "L": 0, "M": 0, "N": 0, "Ñ":0,"O": 0, "P": 0, "Q": 0, "R": 0, "S": 0, "T": 0, "U": 0, "V": 0, "W": 0, "X": 0, "Y": 0, "Z": 0}
     
     porcentajeDeAparicion = {
